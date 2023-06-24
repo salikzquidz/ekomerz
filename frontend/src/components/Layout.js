3af909609cc76466bd5b6f5dd219e9024687d014
salikzquidz <salikinbaik@gmail.com>
2023-06-25 05:08:19 +0800

    mui theme, update product details page, darkMode context

--- frontend/src/components/Layout.js
+++ frontend/src/components/Layout.js
@@ -1,9 +1,19 @@
-import React from "react";
+import React, { useContext } from "react";
 import AppBar from "@mui/material/AppBar";
 import Toolbar from "@mui/material/Toolbar";
 import Typography from "@mui/material/Typography";
-import { Container, Link, styled } from "@mui/material";
+import {
+  Container,
+  CssBaseline,
+  Link,
+  Switch,
+  ThemeProvider,
+  createTheme,
+  styled,
+} from "@mui/material";
 import { Helmet } from "react-helmet";
+import { Store } from "../store/context";
+import Cookies from "js-cookie";
 
 const MyAppBar = styled(AppBar)({
   backgroundColor: "#203040",
@@ -24,38 +34,74 @@ const MyFooter = styled("footer")({
 const MyTypography = styled(Typography)({
   fontWeight: "bold",
   fontSize: "1.5rem",
+  color: "white",
 });
 
 export default function Layout({ title, children }) {
+  const { state, dispatch } = useContext(Store);
+  const { darkMode } = state;
+
+  const theme = createTheme({
+    typography: {
+      h1: {
+        fontSize: "1.6rem",
+        fontWeight: 400,
+        margin: "1rem 0",
+      },
+    },
+    palette: {
+      mode: darkMode ? "dark" : "light",
+      primary: {
+        main: "#f0c000",
+      },
+      secondary: {
+        main: "#108080",
+      },
+    },
+  });
+
+  const darkModeChangeHandler = () => {
+    dispatch({ type: darkMode ? "DARK_MODE_OFF" : "DARK_MODE_ON" });
+    Cookies.set("darkMode", darkMode ? "OFF" : "ON");
+  };
+
   return (
     <div>
       <Helmet>
         <title>{title ? title + " - Ekomerz" : "Ekomerz"}</title>
       </Helmet>
-      <MyAppBar>
-        <Toolbar>
-          <Link href="/" style={{ textDecoration: "none", color: "inherit" }}>
-            <MyTypography>Ekomerz</MyTypography>
-          </Link>
-          <div style={{ flex: 1 }}></div> {/* gap between shop name and link */}
-          <Link
-            href="/cart"
-            style={{ textDecoration: "none", color: "inherit" }}
-          >
-            Cart
-          </Link>
-          <Link
-            href="/login"
-            style={{ textDecoration: "none", color: "inherit" }}
-          >
-            Login
-          </Link>
-        </Toolbar>
-      </MyAppBar>
-      <MyContainer>{children}</MyContainer>
-      <MyFooter>
-        <Typography>All rights reserved. 2023 Ekomerz.</Typography>
-      </MyFooter>
+      <ThemeProvider theme={theme}>
+        <CssBaseline />
+        <MyAppBar>
+          <Toolbar>
+            <Link href="/" style={{ textDecoration: "none", color: "inherit" }}>
+              <MyTypography>Ekomerz</MyTypography>
+            </Link>
+            <div style={{ flex: 1 }}></div>{" "}
+            {/* gap between shop name and link */}
+            <Switch
+              checked={darkMode}
+              onChange={darkModeChangeHandler}
+            ></Switch>
+            <Link
+              href="/cart"
+              style={{ textDecoration: "none", color: "white" }}
+            >
+              Cart
+            </Link>
+            <Link
+              href="/login"
+              style={{ textDecoration: "none", color: "white" }}
+            >
+              Login
+            </Link>
+          </Toolbar>
+        </MyAppBar>
+        <MyContainer>{children}</MyContainer>
+        <MyFooter>
+          <Typography>All rights reserved. 2023 Ekomerz.</Typography>
+        </MyFooter>
+      </ThemeProvider>
     </div>
   );
 }
--- frontend/src/pages/Slug/index.js
+++ frontend/src/pages/Slug/index.js
@@ -2,9 +2,28 @@ import React, { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
 import Layout from "../../components/Layout";
 import axios from "axios";
+import {
+  Button,
+  Card,
+  Grid,
+  Link,
+  List,
+  ListItem,
+  TextField,
+  Typography,
+  styled,
+} from "@mui/material";
+import { Link as ReactRouterLink } from "react-router-dom";
+
+const MyBackToHomepageLink = styled("div")({
+  marginTop: 10,
+  marginBottom: 10,
+  textDecoration: "none",
+});
 
 export default function Slug(props) {
   const [product, setProduct] = useState(null);
+  const [qty, setQty] = useState(1);
   let { id } = useParams();
 
   const getProductDetails = async () => {
@@ -22,5 +41,89 @@ export default function Slug(props) {
     getProductDetails();
   }, []);
 
-  return <Layout title={product?.name}>Slug {product?.name} </Layout>;
+  return (
+    <Layout title={product?.name}>
+      <MyBackToHomepageLink>
+        <Link
+          component={ReactRouterLink}
+          to="/"
+          sx={{ textDecoration: "none" }}
+        >
+          Back to homepage
+        </Link>
+      </MyBackToHomepageLink>
+      <Grid container>
+        <Grid item md={6} xs={12}>
+          <img
+            src={`http://localhost:3300/${product?.image}`}
+            alt={product?.name}
+            width={540}
+            height={540}
+          />
+        </Grid>
+        <Grid item md={3} xs={12}>
+          <List>
+            <ListItem>
+              <Typography variant="h1" component="h1">
+                {product?.name}
+              </Typography>
+            </ListItem>
+            <ListItem>
+              <Typography>Description : {product?.description}</Typography>
+            </ListItem>
+            <ListItem>
+              <Typography>Price : RM {product?.price}</Typography>
+            </ListItem>
+            <ListItem>
+              <Typography>Stock : {product?.countInStock}</Typography>
+            </ListItem>
+            <ListItem>
+              <Typography>Rating : {product?.numReviews}</Typography>
+            </ListItem>
+          </List>
+        </Grid>
+        <Grid item md={3} xs={12}>
+          <Card>
+            <List>
+              <ListItem>
+                <Grid container>
+                  <Grid item xs={6}>
+                    <Typography>Quantity</Typography>
+                  </Grid>
+                  <Grid item xs={6}>
+                    <TextField
+                      type="number"
+                      id="outlined-number"
+                      size="small"
+                      inputProps={{
+                        min: product?.countInStock ? 1 : "Not in stock",
+                        max: product?.countInStock,
+                      }}
+                      value={qty}
+                      onChange={(e) => setQty(e.target.value)}
+                    />
+                  </Grid>
+                </Grid>
+              </ListItem>
+              <ListItem>
+                <Grid container>
+                  <Grid item xs={6}>
+                    <Typography>Price</Typography>
+                  </Grid>
+                  <Grid item xs={6}>
+                    <Typography>RM {product?.price * qty}</Typography>
+                  </Grid>
+                </Grid>
+              </ListItem>
+              <ListItem>
+                <Button fullWidth variant="contained" color="primary">
+                  Add to cart
+                </Button>
+              </ListItem>
+            </List>
+          </Card>
+        </Grid>
+      </Grid>
+    </Layout>
+  );
 }
